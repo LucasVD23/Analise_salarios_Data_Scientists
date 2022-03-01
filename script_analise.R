1c1951d018136d6435ba66b9b74b5a45cc2a8ab5
Lucas D <lucasdomingues2000@hotmail.com>
2022-03-01 20:24:01 -0300

    contabiliza -1s como NA

--- script_analise.R
+++ script_analise.R
@@ -1,11 +1,11 @@
 library(tidyverse)
-ds_salary <- read_csv("{Local do arquivo}")
+ds_salary <- read_csv("D:/Windows/Documentos/PVD/Analise_salarios_Data_Scientists/data_cleaned_2021.csv")
 
 #transformando strings "na" em NAs
 num_faltantes <-c()
 prop_faltantes <- c()
 for(i in 1:ncol(ds_salary)){
-  ds_salary[(ds_salary[,i]=="na"),i] <- NA
+  ds_salary[(ds_salary[,i]=="na" | ds_salary[,i]==-1),i] <- NA
   num_nas =  sum(is.na(ds_salary[,i]))
   num_faltantes <- append(num_faltantes,num_nas)
   prop_faltantes <- append(prop_faltantes,num_nas*100/nrow(ds_salary))
