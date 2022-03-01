ef486025f55b75386d466b5a82fe6660a0e22313
Lucas D <lucasdomingues2000@hotmail.com>
2022-03-01 19:27:32 -0300

    Adiciona tabela para visualizacao da quantidade de dados faltantes

--- script_analise.R
+++ script_analise.R
@@ -1,8 +1,13 @@
 library(tidyverse)
-ds_salary <- read_csv("D:/Windows/Documentos/PVD/Trabalho/data_cleaned_2021.csv")
+ds_salary <- read_csv("{Local do arquivo}")
 
 #transformando strings "na" em NAs
-
+num_faltantes <-c()
+prop_faltantes <- c()
 for(i in 1:ncol(ds_salary)){
   ds_salary[(ds_salary[,i]=="na"),i] <- NA
+  num_nas =  sum(is.na(ds_salary[,i]))
+  num_faltantes <- append(num_faltantes,num_nas)
+  prop_faltantes <- append(prop_faltantes,num_nas*100/nrow(ds_salary))
 }
+faltantes <- tibble(colunas = names(ds_salary),num_faltantes = num_faltantes, prop_faltantes = prop_faltantes)
