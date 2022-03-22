8d7c60d2e2b4452f4e84789b2e52f7ded9ee5c44
Lucas D <lucasdomingues2000@hotmail.com>
2022-03-22 14:50:35 -0300

    Coloca cálculo de correlações em uma função

--- script_analise.R
+++ script_analise.R
@@ -13,13 +13,30 @@ calcular_covariancia <- function(vec1,vec2){
   covariancia <-covariancia/(length(vec1))
 }
 
-calcular_corrs_numericos <- function(vec1,vec2,na.rm=TRUE){
-  coeficientes <- list()
-  r <- calcular_covariancia(vec1,vec2)/(sd(vec1)*sd(vec2))
-  soma_quadrados <- sum((vec1 - mean(vec2)) ^ 2)
-  soma_quadrados_residuais <- sum((vec1 - vec2)^2)
-  r_2 <- 1 - (soma_quadrados_residuais/soma_quadrados) 
-  list(r = r,r_2 = r_2)
+calcular_corrs_numericos <- function(dataset_numeric,na.rm=TRUE){
+  redundantes <- c()
+  for(i in 1:ncol(dataset_numeric)){
+    for(j in 1:ncol(dataset_numeric)){
+      if(i==j||i<j){
+        next
+      }
+      vec1 <- dataset_numeric[,i]
+      vec2 <- dataset_numeric[,j]
+      r <- calcular_covariancia(vec1,vec2)/(sd(vec1)*sd(vec2))
+      soma_quadrados <- sum((vec1 - mean(vec2)) ^ 2)
+      soma_quadrados_residuais <- sum((vec1 - vec2)^2)
+      r_2 <- 1 - (soma_quadrados_residuais/soma_quadrados) 
+      if((r > 0.7 || r < -0.7) || r_2 >0.7){
+        print(paste(names(data_numeric[i])," e ", names(data_numeric[j]),"são correlacionados"))
+        append(redundantes,names(data_numeric[j]))
+        redundantes <- union(redundantes, names(data_numeric[j]))
+        print(paste(r,", ",r_2))
+      }
+    }
+    
+  }
+  return(redundantes)
+
 }
 
 
@@ -72,21 +89,7 @@ data <- select(data,-'Salary Estimate')
 
 data_numeric <- data %>% select_if(is.numeric)
 
-redundantes <- c()
-for (i in 1:ncol(data_numeric)){
-  for(j in 1:ncol(data_numeric)){
-    if(i==j||i<j){
-      next
-    }
-    coefs <- calcular_corrs_numericos(data_numeric[,i],data_numeric[,j])
-    if((coefs$r > 0.7 || coefs$r < -0.7) || coefs$r_2 >0.7){
-      print(paste(names(data_numeric[i])," e ", names(data_numeric[j]),"são correlacionados"))
-      append(redundantes,names(data_numeric[j]))
-      redundantes <- union(redundantes, names(data_numeric[j]))
-      print(paste(coefs$r,", ",coefs$r_2))
-    }
-  }
-}
+redundantes <- calcular_corrs_numericos(data_numeric)
 print(paste0("são redundantes e podem ser removidos: ",toString(redundantes)))
 
 data_nomimal <- data %>% select_if(is.character)
